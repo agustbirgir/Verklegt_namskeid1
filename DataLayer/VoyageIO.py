6abc9e2ebdb6e646756e5d69f011742899ed2819
SiggiMatti <sigurdurmatthias@gmail.com>
2023-12-06 15:05:28 +0000

    Bætti add to voyage

--- DataLayer/VoyageIO.py
+++ DataLayer/VoyageIO.py
@@ -40,9 +40,9 @@ class VoyageIO:
 =======
     def voyage_add_pilot(self, voyage, pilot):
         """Add a pilot to a voyage"""
-        ret_list = []
         pilots = []
 
+<<<<<<< HEAD
 >>>>>>> e603436 (test)
         with open(self.file_name, newline='', encoding='utf-8') as csvfile:
             fieldnames = ["departureFlight", "arrivalFlight", "pilots", "attendants"]
@@ -67,6 +67,46 @@ class VoyageIO:
         ret_list = []
         with open(self.file_name, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
+=======
+        with open(self.file_name, mode='r') as file:
+            reader = csv.DictReader(file)
+>>>>>>> 748767d (Bætti add to voyage)
             for row in reader:
-                ret_list.append(Voyage(row["departureFlight"], row["arrivalFlight"]), row["pilots"], row["attendants"])
-        return ret_list
+                pilots.append(row)
+
+        for row in pilots:
+            if row['name'] == pilot.name:
+                row['pilots'].append(pilot.name) 
+
+        with open(self.file_name, mode='w', newline='') as file:
+            fieldnames = ["departureFlight", "arrivalFlight", "pilots", "attendants"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            # Write headers
+            writer.writeheader()
+
+            # Write updated data
+            writer.writerows(pilots)
+
+    def voyage_add_attendant(self, attendant):
+        """Add a pilot to a voyage"""
+        attendants = []
+
+        with open(self.file_name, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                attendants.append(row)
+
+        for row in attendant:
+            if row['name'] == attendant.name:
+                row['pilots'].append(attendant.name) 
+
+        with open(self.file_name, mode='w', newline='') as file:
+            fieldnames = ["departureFlight", "arrivalFlight", "pilots", "attendants"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            # Write headers
+            writer.writeheader()
+
+            # Write updated data
+            writer.writerows(attendant)
