796c29244faafa685a58149a650e1c9b49af29df
agustbirgir <agustbirgirkarlsson@gmail.com>
2023-12-14 17:18:10 +0000

    sma fix

--- Models/Employee.py
+++ Models/Employee.py
@@ -1,5 +1,5 @@
 class Employee:
-    def __init__(self, name='', profession='', ssn='', homeAddress='', gsmNumber='', email='', homePhone='', status='', scheduled=''):
+    def __init__(self, name='', profession='', ssn='', homeAddress='', gsmNumber='', email='', homePhone='', status='', scheduled='', aircraftLicense=''):
         self.name = name
         self.profession = profession
         self.ssn = ssn
@@ -9,3 +9,4 @@ class Employee:
         self.homePhone = homePhone
         self.status = status
         self.scheduled = scheduled
+        self.aircraftLicense = aircraftLicense
